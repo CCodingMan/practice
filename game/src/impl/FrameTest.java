13208450f631eecd8fbf7a5c021869df19c0a965
CCodingMan <2693986772@qq.com>
2018-06-12 22:51:35 +0800

    模拟台球轨迹

--- game/src/impl/FrameTest.java
+++ game/src/impl/FrameTest.java
@@ -1,3 +1,5 @@
+package impl;
+
 import util.GameUtil;
 
 import java.awt.*;
@@ -6,17 +8,17 @@ import java.awt.event.WindowEvent;
 
 /**
  * @Auther: ljj
- * @Date: 2018/6/11 21:05
- * @Description:窗口测试
+ * @Date: 2018/6/12 21:51
+ * @Description:模拟台球轨迹
  */
-public class GameFrame extends Frame{
+public class FrameTest extends Frame{
     /**
      * 加载图片
      */
     private Image image = GameUtil.getImage("images/sun.png");
 
     public static void main(String[] args) {
-        GameFrame gf = new GameFrame();
+        FrameTest gf = new FrameTest();
         gf.launchFrame();
     }
 
@@ -28,7 +30,7 @@ public class GameFrame extends Frame{
         setLocation(100,100);
         setVisible(true);
 
-        new PainThread().start();//启动
+        new FrameTest.PainThread().start();//启动
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -38,59 +40,42 @@ public class GameFrame extends Frame{
         });
     }
 
-    private int x = 100;
-    private int y = 100;
+    private double x = 100;
+    private double y = 100;
+    private double degree = 3.14/3;//[0-2派]
+    private double speed = 10;
     @Override
     public void paint(Graphics g) {
-        //画线
-        g.drawLine(100, 100, 200, 200);
-        //画圆
-        g.drawOval(100, 100, 200, 200);
-        //画矩形
-        g.drawRect(100, 100, 200, 200);
-        //字体设置
-        Font f = new Font("微软雅黑", Font.BOLD, 15);
-        g.setFont(f);
-        //画笔颜色
-        g.setColor(Color.red);
-        //画字
-        g.drawString("chinagoodman", 200, 200);
+        if(speed > 0){
+            speed -= 0.02;
+        }else{
+            speed = 0;
+        }
 
-        g.setColor(Color.black);
-        //填充矩形
-        g.fillRect(100, 100, 50, 50);
-        Color c = g.getColor();
-        //填充圆形
-        g.fillOval(300, 300, 50,50);
-        //还原颜色
-        g.setColor(c);
         //画图片
-        g.drawImage(image, x, y, 20, 20, null);
-        if(x <= 300){
-            x++;
-        }else{
-            x = 100;
+        g.drawImage(image, (int) x, (int) y, 20, 20, null);
+        x += speed*Math.cos(degree);
+        y += speed*Math.sin(degree);
+        if(y > 500 - 30 || y < 30){
+            degree = -degree;
         }
-
-        if(x <= 300){
-            y++;
-        }else{
-            y = 100;
+        if(x > 500 - 30 || x < 30){
+            degree = Math.PI - degree;
         }
-
     }
 
     /**
      * 重画窗口
      */
     class PainThread extends Thread {
+        @SuppressWarnings("Duplicates")
         @Override
         public void run() {
             //noinspection InfiniteLoopStatement
             while(true){
                 repaint();
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(40);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
