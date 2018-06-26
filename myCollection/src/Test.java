7259524fee799889897f89fdace5b993002e1b65
CCodingMan <2693986772@qq.com>
2018-06-26 23:44:01 +0800

    简单实现ArrayList

---
+++ myCollection/src/Test.java
@@ -0,0 +1,28 @@
+import com.ljj.arraylist.MyArrayList;
+
+/**
+ * @Auther: ljj
+ * @Date: 2018/6/26 21:54
+ * @Description:测试
+ */
+public class Test {
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("k");
+        list.add("l");
+        list.add("m");
+        list.add("n");
+
+        System.out.println(list.isEmpty());
+        System.out.println(list.remove(10));
+        System.out.println(list.size());
+    }
+
+}
---
+++ myCollection/src/com/ljj/arraylist/MyArrayList.java
@@ -0,0 +1,69 @@
+package com.ljj.arraylist;
+
+/**
+ * @Auther: ljj
+ * @Date: 2018/6/26 21:55
+ * @Description:模仿实现ArrayList
+ */
+public class MyArrayList<E> {
+    private int size;
+    private Object[] elementData;
+
+    public MyArrayList() {
+        this(10);
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if(initialCapacity > 0){
+            this.elementData = new Object[initialCapacity];
+        }else if(initialCapacity == 0){
+            this.elementData = new Object[]{};
+        }else{
+            throw new IllegalArgumentException("Illegal capacity:"+initialCapacity);
+        }
+    }
+
+    public void add(Object obj){
+        if(size >= elementData.length){
+            Object[] newArray = new Object[size*2+1];
+            System.arraycopy(elementData,0,newArray,0,elementData.length);
+            this.elementData = newArray;
+        }
+        elementData[size++] = obj;
+    }
+
+    public Object get(int index){
+        checkIndex(index);
+        return elementData[index];
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public E remove(int index){
+        checkIndex(index);
+        E oldValue = elementData(index);
+        int numMoved = size - index -1;
+        if(numMoved > 0){
+            System.arraycopy(elementData,index+1,elementData,index,numMoved);
+        }
+        this.elementData[--size] = null;
+        return oldValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E elementData(int index){
+        return (E)elementData[index];
+    }
+
+    public void checkIndex(int index){
+        if(index < 0 || index+1 > elementData.length){
+            throw new IllegalArgumentException("illegal index:"+index);
+        }
+    }
+}
